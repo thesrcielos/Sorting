d7c3d0c916c886cd1bdce5136b343a0a595fbf3a
thesrcielos <diegomadi75@gmail.com>
2025-03-20 19:42:05 -0500

    Add memory usage

--- app.py
+++ app.py
@@ -2,41 +2,11 @@ import sys
 from sorting import algorithms
 from sorting import execution_time_gathering
 import matplotlib.pyplot as plt
-import numpy as np
 
-def graphicsQuickHeap():
-    minimum_size = 100000
-    maximum_size = 1000000
-    step = 50000
-    samples_by_size = 5
-    table = execution_time_gathering.take_execution_time(minimum_size, maximum_size, step, samples_by_size, True)
-    print("Size | Quick sort | Heap sort")
-    for row in table:
-        print(row)
-    data = np.array(table)
-
-    # La primera columna será el eje X
-    x = data[:, 0]
-    # Las siguientes columnas serán los valores de Y
-    y1 = data[:, 1]
-    y2 = data[:, 2]
-
-    # Crear el gráfico
-    plt.plot(x, y1, label="Quick", color='green')
-    plt.plot(x, y2, label="Heap", color='red')
-
-    plt.title('Comparation Sorting algorithms')
-    plt.xlabel('X')
-    plt.ylabel('Y')
-
-    plt.legend()
-
-    plt.show()
-
-if __name__ == "__main__":
+def graphicsAlgorithms():
     minimum_size = 1000
-    maximum_size = 10000
-    step = 500
+    maximum_size = 5000
+    step = 1000
     samples_by_size = 5
     
     table = execution_time_gathering.take_execution_time(minimum_size, maximum_size, step, samples_by_size)
@@ -46,14 +16,12 @@ if __name__ == "__main__":
         print(row)
 
 
-    data = np.array(table)
-
     # La primera columna será el eje X
-    x = data[:, 0]
+    x = [row[0] for row in table]
     # Las siguientes columnas serán los valores de Y
-    y1 = data[:, 1]
-    y2 = data[:, 2]
-    y3 = data[:, 3]
+    y1 = [row[1][0] for row in table]
+    y2 = [row[2][0] for row in table]
+    y3 = [row[3][0] for row in table]
 
     # Crear el gráfico
     plt.plot(x, y1, label="Selection", color='blue')
@@ -61,8 +29,64 @@ if __name__ == "__main__":
     plt.plot(x, y3, label="Heap", color='red')
 
     plt.title('Comparation Sorting algorithms')
-    plt.xlabel('X')
-    plt.ylabel('Y')
+    plt.xlabel('Size')
+    plt.ylabel('Time')
     plt.legend()
     plt.show()
+
+    y1 = [row[1][1] for row in table]
+    y2 = [row[2][1] for row in table]
+    y3 = [row[3][1] for row in table]
+
+    plt.plot(x, y1, label="Selection", color='blue')
+    plt.plot(x, y2, label="Quick", color='green')
+    plt.plot(x, y3, label="Heap", color='red')
+
+    plt.title('Comparation Sorting algorithms Memory Usage')
+    plt.xlabel('Size')
+    plt.ylabel('Memory')
+    plt.legend()
+    plt.show()
+
+def graphicsQuickHeap():
+    minimum_size = 100000
+    maximum_size = 500000
+    step = 50000
+    samples_by_size = 5
+    table = execution_time_gathering.take_execution_time(minimum_size, maximum_size, step, samples_by_size, True)
+    print("Size | Quick sort | Heap sort")
+    for row in table:
+        print(row)
+
+    # La primera columna será el eje X
+    x = [row[0] for row in table]
+    # Las siguientes columnas serán los valores de Y
+    y1 = [row[1][0] for row in table]
+    y2 = [row[2][0] for row in table]
+
+    # Crear el gráfico
+    plt.plot(x, y1, label="Quick", color='blue')
+    plt.plot(x, y2, label="Heap", color='green')
+
+    plt.title('Comparation Sorting algorithms')
+    plt.xlabel('Size')
+    plt.ylabel('Time')
+    plt.legend()
+    plt.show()
+
+    y1 = [row[1][1] for row in table]
+    y2 = [row[2][1] for row in table]
+
+    plt.plot(x, y1, label="Selection", color='blue')
+    plt.plot(x, y2, label="Quick", color='green')
+
+    plt.title('Comparation Sorting algorithms Memory Usage')
+    plt.xlabel('Size')
+    plt.ylabel('Memory')
+    plt.legend()
+    plt.show()
+
+
+if __name__ == "__main__":
+    graphicsAlgorithms()
     graphicsQuickHeap()
